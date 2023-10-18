6bf6005600462d0760bc8fafeeb3ffa282e197cb
gsinghpawar25 <106743563+gsinghpawar25@users.noreply.github.com>
2023-10-18 11:36:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 streamlit.title("My parents new healthy diner")
+streamlit.text("Omega 3 and Blueberry Oatmeal")
 streamlit.text("Kale,Spinach and Rocket Smmothie")
 streamlit.text("OHard Boiled free range egg")
 
